fe94a71d1f4b81b4afb9603f08d91c5896155e96
Johan Lidberg <johan.lidberg@skola.botkyrka.se>
2020-11-19 10:41:49 +0100

    47

--- 47/1/script.js
+++ 47/1/script.js
@@ -1,8 +1,9 @@
 
-// låt i vara 0
+let i = 0
 
-// while i < 6
-//      logga ut i
-//      öka i med 1
+while (i < 6){
+     console.log(i)
+     i++
+}
 
-// logga ut "Färdig!"
+console.log("färdig")
---
+++ 47/2/script.js
@@ -0,0 +1,12 @@
+const sumThree = (x, y, z) => {
+x + y + z
+} 
+sumThree(2, 3, 5)
+
+
+const isZero = (n) => n === 0
+
+isZero(3)
+
+
+
